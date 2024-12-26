3bcc8df5ac47c3de98cb7351a7c06ab2c48806cb
pophero110 <ojn2014520@gmail.com>
2024-12-25 19:14:07 -0600

    remove unused variables

--- src/app/components/StyledMarkdown.tsx
+++ src/app/components/StyledMarkdown.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import mermaid from "mermaid";
@@ -16,18 +15,18 @@ const StyledMarkdown = ({ content }: { content: string }) => {
   return (
     <ReactMarkdown
       components={{
-        h1: ({ node, ...props }) => (
+        h1: ({ ...props }) => (
           <h1 className="text-2xl font-bold my-4" {...props} />
         ),
-        h2: ({ node, ...props }) => (
+        h2: ({ ...props }) => (
           <h2 className="text-1xl font-bold my-4" {...props} />
         ),
-        p: ({ node, ...props }) => <p className="my-2" {...props} />,
-        ul: ({ node, ...props }) => (
+        p: ({ ...props }) => <p className="my-2" {...props} />,
+        ul: ({ ...props }) => (
           <ul className="list-disc list-inside my-2" {...props} />
         ),
-        li: ({ node, ...props }) => <li className="ml-4" {...props} />,
-        code: ({ node, inline, className, children, ...props }) => {
+        li: ({ ...props }) => <li className="ml-4" {...props} />,
+        code: ({ inline, className, children, ...props }) => {
           const match = /language-(\w+)/.exec(className || "");
           return !inline && match && match[1] === "mermaid" ? (
             <Mermaid chart={String(children).replace(/\n$/, "")} />
